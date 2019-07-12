2204f740854bee79a56326b1fb3fe08fadf37925
Zeke Hart <heirloch@gmail.com>
2019-07-12 17:00:37 -0400

    adds additional sorting and searching methods. also stuff doesn't show up untila fter the first search

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const searchButton = document.querySelector('#search-button')
 const searchBar = document.querySelector('#search-bar')
 const searchForm = document.querySelector('#search-form')
 const audioPlayer = document.querySelector('#music-player')
+const displayMore = document.querySelector('.more-results')
+const subHeader = document.querySelector(".subheader")
+const songFilter = document.querySelector(".songs-search")
+const albumFilter = document.querySelector(".albums-search")
+const bandFilter = document.querySelector(".bands-search")
+let searchType = 'songs'
 
 searchForm.addEventListener('submit', function (event) {
     event.preventDefault()
@@ -14,12 +20,73 @@ searchForm.addEventListener('submit', function (event) {
             populateResults(data.results)
 
         })
+    displayMore.style.display = 'inline'
+    subHeader.style.display = 'block'
+    document.querySelector(".results-type").style.display = 'inline'
 })
 
-function populateResults(rawResults) {
+displayMore.addEventListener('click', function () {
+    let searchUrl = makeUrl(searchBar.value)
+    displayMore.style.display = 'none'
+    fetch(searchUrl)
+        .then(response => response.json()
+        )
+        .then(function (data) {
+            console.log(data)
+            populateResults(data.results, 10)
+
+        })
+})
+
+bandFilter.addEventListener('click', function () {
+    searchType = 'bands'
+    let searchUrl = makeUrl(searchBar.value)
+    fetch(searchUrl)
+        .then(response => response.json()
+        )
+        .then(function (data) {
+            console.log(data)
+            populateResults(data.results)
+
+        })
+    displayMore.style.display = 'inline'
+})
+
+songFilter.addEventListener('click', function () {
+    searchType = 'songs'
+    let searchUrl = makeUrl(searchBar.value)
+    fetch(searchUrl)
+        .then(response => response.json()
+        )
+        .then(function (data) {
+            console.log(data)
+            populateResults(data.results)
+
+        })
+    displayMore.style.display = 'inline'
+})
+
+albumFilter.addEventListener('click', function () {
+    searchType = 'albums'
+    let searchUrl = makeUrl(searchBar.value)
+    console.log(searchUrl)
+    fetch(searchUrl)
+        .then(response => response.json()
+        )
+        .then(function (data) {
+            console.log(data)
+            populateResults(data.results)
+
+        })
+    displayMore.style.display = 'inline'
+})
+
+function populateResults(rawResults, secondArg) {
     const displaySection = document.querySelector('#results-display')
     displaySection.innerHTML = ''
-    rawResults.length = Math.min(rawResults.length, 16)
+    if (!secondArg) {
+        rawResults.length = Math.min(rawResults.length, 16)
+    }
     for (let track of rawResults) {
         displaySection.appendChild(makeBox(track))
     }
@@ -27,23 +94,50 @@ function populateResults(rawResults) {
 
 function makeBox(trackInfo) {
     const trackDiv = document.createElement('div')
+
     trackDiv.classList.add('track', 'fl', 'center', 'w-50', 'w-25-ns', 'tc', 'ma2')
-    trackDiv.innerHTML = `
-    <img class="mt2" src="${trackInfo.artworkUrl100}">
-    <h3 class="red">${trackInfo.trackName}</h3>
-    <h4 class="red">${trackInfo.artistName}</h4>
-    <button class="play-preview mb2 bg-pink o-80 red bw0 pa1 br3 link dim mb2 dib white" data-preview="${trackInfo.previewUrl}">Play preview</button>
-    `
+    if (searchType === 'songs') {
+        if (trackInfo.trackName.length >= 30) {
+            trackInfo.trackName = trackInfo.trackName.substring(0, 30) + "..."
+        }
+        trackDiv.innerHTML = `
+        <img class="mt2" src="${trackInfo.artworkUrl100}">
+        <h3 class="hot-pink">${trackInfo.trackName}</h3>
+        <h4 class="hot-pink">${trackInfo.artistName}</h4>
+        <button class="play-preview mb2 bg-pink o-80 red bw0 pa1 br3 link dim mb2 dib white" data-preview="${trackInfo.previewUrl}">Play preview</button>
+        `
+    }
+    if (searchType === 'albums') {
+        trackInfo.releaseDate = trackInfo.releaseDate.substring(0, 7)
+        trackDiv.innerHTML = `
+        <img class="mt2" src="${trackInfo.artworkUrl100}">
+        <h3 class="hot-pink">${trackInfo.collectionName}</h3>
+        <h4 class="hot-pink">${trackInfo.artistName}</h4>
+        <h4 class="hot-pink">${trackInfo.releaseDate}</h4>
+        `
+    }
+    if (searchType === 'bands') {
+        trackDiv.innerHTML = `
+        <h2 class="hot-pink">${trackInfo.artistName}</h2>
+        <h3 class="hot-pink">${trackInfo.primaryGenreName}</h3>
+        `
+    }
     return trackDiv
 }
 
 function makeUrl(searchText) {
     let uriConverted = encodeURIComponent(searchText)
-    return `https://itunes-api-proxy.glitch.me/search?term=${uriConverted}&media=music&entity=song&attribute=artistTerm`
-
+    // let entity = 
+    if (searchType === 'songs') {
+        return `https://itunes-api-proxy.glitch.me/search?term=${uriConverted}&media=music&entity=song&attribute=songTerm`
+    }
+    else if (searchType === 'bands') {
+        return `https://itunes-api-proxy.glitch.me/search?term=${uriConverted}&media=music&entity=musicArtist&attribute=artistTerm`
+    }
+    else if (searchType === 'albums') {
+        return `https://itunes-api-proxy.glitch.me/search?term=${uriConverted}&media=music&entity=album&attribute=albumTerm`
+    }
 }
-
-
 
 document.querySelector('#results-display').addEventListener('click', function (event) {
     if (event.target && event.target.matches('button.play-preview')) {
